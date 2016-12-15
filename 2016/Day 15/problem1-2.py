82afade2a6c8fa21e66e7afddd4faafa65aeb9c5
jhegele <john.hegele@gmail.com>
2016-12-15 06:47:23 -0500

    Add 2016 Day 15

---
+++ 2016/Day 15/problem1-2.py
@@ -0,0 +1,48 @@
+"""
+Problem input:
+Disc #1 has 7 positions; at time=0, it is at position 0.
+Disc #2 has 13 positions; at time=0, it is at position 0.
+Disc #3 has 3 positions; at time=0, it is at position 2.
+Disc #4 has 5 positions; at time=0, it is at position 2.
+Disc #5 has 17 positions; at time=0, it is at position 0.
+Disc #6 has 19 positions; at time=0, it is at position 7.
+"""
+
+# Flag to enable part 2
+part2 = False
+
+class Disc(object):
+  """Object to represent a disc with a given order, total number of positions,
+  and the value of the current position.
+  """
+
+  def __init__(self, order, total_pos, curr_pos):
+    self.order = order
+    self.total_pos = total_pos
+    self.curr_pos = curr_pos
+
+  def check(self, t):
+    """Pass in the time that the capsule is released and check if thie
+    disc is in position 0 at the time the capsule reaches it."""
+    return (t + self.order + self.curr_pos) % self.total_pos == 0
+
+# Our array of discs
+discs = [
+  Disc(1, 7, 0),
+  Disc(2, 13, 0),
+  Disc(3, 3, 2),
+  Disc(4, 5, 2),
+  Disc(5, 17, 0),
+  Disc(6, 19, 7)
+]
+
+# Add a 7th disc for part 2
+if part2:
+  discs.append(Disc(7, 11, 0))
+
+t = 0
+# Brute force is and iterate over time until everything is aligned
+while not all([d.check(t) for d in discs]):
+  t += 1
+
+print('Press the button at t={0}.'.format(t))
